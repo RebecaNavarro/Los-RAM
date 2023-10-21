943f2757ecead2d628a3c2bc5d2ee5ee6e9b24ab
Mariana <delarroyorengelmariana@gmail.com>
2023-10-21 19:46:15 -0400

    Commit pantalla crear cuenta

---
+++ app/src/main/java/com/example/losram/crearcuenta.kt
@@ -0,0 +1,11 @@
+package com.example.losram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class crearcuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crearcuenta)
+    }
+}
