2fca90e08147e74a09f455f810ba762007107c5e
adrian <adrian.sanchez.nina@gmail.com>
2023-10-22 15:11:19 -0400

    bantalla basica de Favoritos

---
+++ app/src/main/java/com/example/losram/favoritos.kt
@@ -0,0 +1,11 @@
+package com.example.losram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class favoritos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favoritos)
+    }
+}
