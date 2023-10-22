a8bde7965cd05f8b7dcec18730a9938297a9fbcc
Mariana <delarroyorengelmariana@gmail.com>
2023-10-21 20:13:00 -0400

    Commit themes botones

---
+++ app/src/main/java/com/example/losram/Para_ellas_seccion.kt
@@ -0,0 +1,11 @@
+package com.example.losram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Para_ellas_seccion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_para_ellas_seccion)
+    }
+}
