b3f26f62d86d2642e95bd4b35b0114dca4b067ec
Beca <becanavarro2003@gmail.com>
2023-10-21 21:48:45 -0400

    commit de pantallas de los estados del pedido, pantalla de gracias, pantalla de pago tarjeta, pantalla de puntuacion, pantalla

---
+++ app/src/main/java/com/example/losram/EstadoPedidoDeliveryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.losram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EstadoPedidoDeliveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_estado_pedido_delivery)
+    }
+}
---
+++ app/src/main/java/com/example/losram/GraciasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.losram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GraciasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gracias)
+    }
+}
