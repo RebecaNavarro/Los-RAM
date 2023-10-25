ceb92a384c48b8a961959b8ebc7d6d5ed354bb8b
Beca <becanavarro2003@gmail.com>
2023-10-25 16:28:39 -0400

    commit de temas con tints y algunos comentarios

--- app/src/main/java/com/example/losram/PantallaCargaActivity.kt
+++ app/src/main/java/com/example/losram/PantallaCargaActivity.kt
@@ -1,11 +1,43 @@
 package com.example.losram
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.example.losram.databinding.ActivityCrearcuentaBinding
+import com.example.losram.databinding.ActivityPantallaCargaBinding
+import com.example.losram.databinding.ActivityPantalladeinicioBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class PantallaCargaActivity : AppCompatActivity() {
+
+    val context: Context = this
+    val activity: Activity = this
+
+    //lateinit var binding: ActivityPantallaCargaBinding
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+       // binding= ActivityPantallaCargaBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_pantalla_carga)
+
+        imageView = findViewById(R.id.fondo_super_chero)
+
+        imageView.setOnClickListener{
+            val intent: Intent = Intent(context, PantallaDeInicioActivity::class.java)
+            startActivity(intent)
+        }
+
+//        binding.fondoSuperChero.setOnClickListener{
+//            val intent: Intent = Intent(context, PantallaDeInicioActivity::class.java)
+//            startActivity(intent)
+//        }
     }
+
+
+
 }
--- app/src/main/java/com/example/losram/PantallaDeInicioActivity.kt
+++ app/src/main/java/com/example/losram/PantallaDeInicioActivity.kt
@@ -5,30 +5,51 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.losram.databinding.ActivityCrearcuentaBinding
+import com.example.losram.databinding.ActivityPantallaCargaBinding
 import com.example.losram.databinding.ActivityPantalladeinicioBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class pantalladeinicio : AppCompatActivity() {
+class PantallaDeInicioActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPantalladeinicioBinding
     private lateinit var auth: FirebaseAuth
+    lateinit var textView: TextView
 
     val context: Context = this
     val activity: Activity = this
-class PantallaDeInicioActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalladeinicio)
+
+        textView = findViewById(R.id.crearcuenta)
+
+        textView.setOnClickListener{
+            val intent: Intent = Intent(context, CrearCuentaActivity::class.java)
+            startActivity(intent)
+        }
+
+
+//        setContentView(binding.root)
+//
+//        binding = ActivityPantalladeinicioBinding.inflate(layoutInflater)
+//        binding.crearcuenta.setOnClickListener {
+//            val intent: Intent = Intent(context, CrearCuentaActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 
     fun loginUser(email:String, password:String){
         auth.signInWithEmailAndPassword(email,password)
             .addOnCompleteListener{task ->
                 if(task.isSuccessful){
-                    val intent: Intent = Intent(context, pantallaprincipal::class.java)
+                    val intent: Intent = Intent(context, PantallaPrincipalActivivy::class.java)
                     startActivity(intent)
                 }else{}
             }
     }
+
 }
