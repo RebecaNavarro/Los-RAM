3220caf73fddfbb5f86ce54896cccee075d03736
Beca <becanavarro2003@gmail.com>
2023-10-26 09:23:30 -0400

    commit de mapa

--- app/src/main/java/com/example/losram/UbicacionesActivity.kt
+++ app/src/main/java/com/example/losram/UbicacionesActivity.kt
@@ -2,11 +2,24 @@ package com.example.losram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.example.losram.R
+import com.example.losram.databinding.ActivityCrearcuentaBinding
+import com.example.losram.databinding.ActivityUbicacionesBinding
+import com.example.losram.fragment.MapaFragment
 
 class UbicacionesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUbicacionesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityUbicacionesBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ubicaciones)
+        setContentView(binding.root)
+        val fragment = MapaFragment()
+        supportFragmentManager.commit {
+            replace(binding.mapa.id, fragment)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
     }
 }
