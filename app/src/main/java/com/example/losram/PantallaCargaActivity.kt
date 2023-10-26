992aaab8d16844ff6c6b509e6e8bb60cfa32c59d
Beca <becanavarro2003@gmail.com>
2023-10-26 08:02:44 -0400

    commit para el login que si nos funciona y pantalla con timer

--- app/src/main/java/com/example/losram/PantallaCargaActivity.kt
+++ app/src/main/java/com/example/losram/PantallaCargaActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import com.example.losram.databinding.ActivityPantallaCargaBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -16,18 +17,37 @@ class PantallaCargaActivity : AppCompatActivity() {
     val activity: Activity = this
 
     lateinit var binding: ActivityPantallaCargaBinding
-    //lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityPantallaCargaBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.fondoSuperChero.setOnClickListener{
-            val intent: Intent = Intent(context, PantallaDeInicioActivity::class.java)
+        val handler = android.os.Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, PantallaDeInicioActivity::class.java)
             startActivity(intent)
-        }
+        }, 2000)
     }
+//        binding.fondoSuperChero.setOnClickListener{
+//            val intent: Intent = Intent(context, PantallaDeInicioActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 
+//    class MainActivity : AppCompatActivity() {
+//        private lateinit var binding: ActivityMainBinding
+//        val context: Context = this
+//        override fun onCreate(savedInstanceState: Bundle?) {
+//            super.onCreate(savedInstanceState)
+//            binding = ActivityMainBinding.inflate(layoutInflater)
+//            setContentView(binding.root)
+//            val handler = android.os.Handler(Looper.getMainLooper())
+//            handler.postDelayed({
+//                val intent = Intent(this, LoginActivity::class.java)
+//                startActivity(intent)
+//                finish()
+//            }, 2000)
+//        }
+//    }
 
 
 }
