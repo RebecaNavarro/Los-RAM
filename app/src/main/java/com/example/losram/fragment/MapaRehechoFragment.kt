e6d2f3c74e9eab04938ce4a3be82c6a1e5606b82
Beca <becanavarro2003@gmail.com>
2023-10-27 09:39:22 -0400

    Commit de los mapas rehecho con los marcadores

--- app/src/main/java/com/example/losram/fragment/MapaRehechoFragment.kt
+++ app/src/main/java/com/example/losram/fragment/MapaRehechoFragment.kt
@@ -31,6 +31,60 @@ class MapaRehechoFragment : Fragment() {
 
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+//       val lapaz = LatLng(-16.541410, -68.077619)
+//        googleMap.addMarker(MarkerOptions().position(lapaz).title("San Miguel La Paz"))
+//1
+//        val bigSur = LatLng(-16.540395, -68.077636)
+//        googleMap.addMarker(MarkerOptions().position(bigSur).title("Big Sur San Miguel"))
+////2
+//        val hering = LatLng(-16.543373, -68.077334)
+//        googleMap.addMarker(MarkerOptions().position(hering).title("Hering San Miguel"))
+////3
+//        val clementine = LatLng(-16.540060, -68.079662)
+//        googleMap.addMarker(MarkerOptions().position(clementine).title("Clementine San Miguel"))
+////4
+//        val somos = LatLng(-16.536610, -68.075652)
+//        googleMap.addMarker(MarkerOptions().position(somos).title("Somos Calacoto"))
+////5
+//        val mango = LatLng(-16.541472, -68.079472)
+//        googleMap.addMarker(MarkerOptions().position(mango).title("Mango San Miguel"))
+////6
+//        val benetton = LatLng(-16.541324, -68.080728)
+//        googleMap.addMarker(MarkerOptions().position(benetton).title("Benetton San Miguel"))
+////7
+//        val faceBoutique = LatLng(-16.541000, -68.078528)
+//        googleMap.addMarker(MarkerOptions().position(faceBoutique).title("FaceBoutique San Miguel"))
+////8
+//        val vickys = LatLng(-16.544653, -68.077783)
+//        googleMap.addMarker(MarkerOptions().position(vickys).title("Vickys Casual San Miguel"))
+////9
+//        val yuth = LatLng(-16.539611, -68.077806)
+//        googleMap.addMarker(MarkerOptions().position(yuth).title("Yuth San Miguel"))
+////10
+//        val fairplay = LatLng(-16.545417, -68.078750)
+//        googleMap.addMarker(MarkerOptions().position(fairplay).title("FairPlay San Miguel"))
+////11
+//        val eye = LatLng(-16.542330, -68.077246)
+//        googleMap.addMarker(MarkerOptions().position(eye).title("E&E San Miguel"))
+////12
+//        val womansecret = LatLng(-16.542330, -68.077246)
+//        googleMap.addMarker(MarkerOptions().position(womansecret).title("WomanSecret San Miguel"))
+////13
+//        val solmania = LatLng(-16.542539, -68.080952)
+//        googleMap.addMarker(MarkerOptions().position(solmania).title("Solmania San Miguel"))
+////14
+//        val lupo = LatLng(-16.543278, -68.077222)
+//        googleMap.addMarker(MarkerOptions().position(lupo).title("LUPO San Miguel"))
+////15
+//        val textilon = LatLng(-16.541722, -68.077500)
+//        googleMap.addMarker(MarkerOptions().position(textilon).title("Textilon San Miguel"))
+////16
+//        val surmoda = LatLng(-16.540265, -68.078691)
+//        googleMap.addMarker(MarkerOptions().position(surmoda).title("Sur Moda San Miguel"))
+//////17
+//        val adidas = LatLng(-16.540211, -68.078346)
+//        googleMap.addMarker(MarkerOptions().position(adidas).title("Adidas San Miguel"))
     }
 
     override fun onCreateView(
