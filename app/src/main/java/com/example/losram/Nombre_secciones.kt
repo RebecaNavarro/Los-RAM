682b503434d5540c07b7c3e3e64dc5951c5d89e7
Mariana <delarroyorengelmariana@gmail.com>
2023-10-26 21:56:49 -0400

    Commit lista de tiendas en RecyclerViewTiendasActivity

---
+++ app/src/main/java/com/example/losram/Nombre_secciones.kt
@@ -0,0 +1,11 @@
+package com.example.losram
+
+enum class Nombre_secciones {
+
+    FORMAL,
+    DE_DIARIO,
+    DEPORTIVO,
+    TROPICAL,
+    PARA_DORMIR,
+    ROPA_INTERIOR
+}
--- app/src/main/java/com/example/losram/dataclases/Tiendas.kt
+++ app/src/main/java/com/example/losram/dataclases/Tiendas.kt
@@ -1,11 +1,13 @@
 package com.example.losram.dataclases
 
+import com.example.losram.Nombre_secciones
+
 data class Tiendas(
     val nombreTienda:String,
     val puntuacionTienda:Double,
     val imagenPrenda:Int,
     val direccion:String,
     val estadoDeTienda:String,
-    val tipo: String
+    val tipo: List<Nombre_secciones>
 )
 
